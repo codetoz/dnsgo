19049c28ba3f152bcdd7691fd83040a21ca01902
Mahdi Darabi <mahdidarabi7@yahoo.com>
2024-03-26 01:06:06 +0330

    feat(list): Improve preferences of output

--- internal/list/handle_list.go
+++ internal/list/handle_list.go
@@ -6,6 +6,7 @@ import (
 	"text/tabwriter"
 
 	"github.com/fatih/color"
+	"golang.org/x/term"
 )
 
 func Handle() {
@@ -15,10 +16,18 @@ func Handle() {
 
 	w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.TabIndent)
 
-	fmt.Fprintf(w, "%s\t%s\t%s\n", green("Name"), white("IPs"), red("Address"))
+	termWidth, _, _ := term.GetSize(0)
 
-	for _, option := range DnsOptions {
-		fmt.Fprintf(w, "%s\t%s\t%s\n", green(option.Name), white(option.IPs), red(option.Address))
+	if termWidth > 80 {
+		fmt.Fprintf(w, "%s\t%s\t%s\n", green("Name"), white("IPs"), red("Address"))
+		for _, option := range DnsOptions {
+			fmt.Fprintf(w, "%s\t%s\t%s\n", green(option.Name), white(option.IPs), red(option.Address))
+		}
+	} else {
+		fmt.Fprintf(w, "%s\t\t\t%s\n", green("Name"), white("IPs"))
+		for _, option := range DnsOptions {
+			fmt.Fprintf(w, "%s\t\t\t%s\n", green(option.Name), white(option.IPs))
+		}
 	}
 
 	w.Flush()
